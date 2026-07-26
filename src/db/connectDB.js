b7816f1fcd5c88cb6672f762f2f4aef94cba42cb
ziakha12 <kzzia06@gmail.com>
2026-07-27 00:45:27 +0500

    fix errors

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `mongodb+srv://zia:Pakistan@cluster0.50kk6vj.mongodb.net/fyp`
+      `${process.env.MONGODB_URI}`
     );
     console.log(
       `\n MongoDB connected! DB Host: ${connectionInstance.connection.host}`
